b7662afc704d223f75c855807eba14b12c090fe6
Oleksandr Melnyk <Oleksandr_Melnyk2@epam.com>
2020-02-16 21:22:53 +0200

    Fixed artifacts path

--- build.cmd
+++ build.cmd
@@ -10,6 +10,6 @@ call dotnet test src/Standart.Hash.xxHash.Test
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo Packing Standart.Hash.xxHash
-call dotnet pack src/Standart.Hash.xxHash -c Release -o ..\artifacts
+call dotnet pack src/Standart.Hash.xxHash -c Release -o .\artifacts
 
 popd
